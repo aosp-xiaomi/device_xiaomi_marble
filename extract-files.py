1a71d6f0debca363a6ef762f7b0c30faf739dcf1
Arian <arian.kulmer@web.de>
2022-06-22 16:31:14 +0200

    marble: Bring back face detection libs
    Change-Id: I50af944e9398eef352f72f4729999f8d0c556837
    Signed-off-by: Rasenkai <rasenkai99@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -56,6 +56,10 @@ blob_fixups: blob_fixups_user_type = {
     .clear_symbol_version('AHardwareBuffer_lockPlanes')
     .clear_symbol_version('AHardwareBuffer_release')
     .clear_symbol_version('AHardwareBuffer_unlock'),
+    ('vendor/lib64/hw/camera.qcom.so', 'vendor/lib64/libFaceDetectpp-0.5.2.so', 'vendor/lib64/libfacedet.so'): blob_fixup()
+        .binary_regex_replace(b'libmegface.so', b'libfacedet.so')
+        .binary_regex_replace(b'libMegviiFacepp-0.5.2.so', b'libFaceDetectpp-0.5.2.so')
+        .binary_regex_replace(b'megviifacepp_0_5_2_model', b'facedetectpp_0_5_2_model'),
 }
 
 module = ExtractUtilsModule(
